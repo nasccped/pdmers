d4636e0a684a8f999cfe7a9a0b63e64e600435ae
Nasccped <pedro.main.tec@gmail.com>
2025-09-12 21:32:17 -0300

    chore: utils module

---
+++ src/utils/mod.rs
@@ -0,0 +1,23 @@
+//! Utilities for the `pdmers` program.
+
+mod printable;
+mod runnable_item;
+
+/// # Printing utilities
+///
+/// Provides a set of printing features, such as:
+/// - a common struct that holds the printing responsibility ([`Printer`])
+/// - a common trait that allows complex items (`struct`, `enum`, `...`) to be printed
+///   ([PrintableItem])
+/// - a file enum to redirect printing output ([`StdWhere`])
+/// - a set of tags for pretty fast printing ([`PrintableTag`])
+pub mod print {
+    pub use super::printable::{PrintableItem, PrintableTag, Printer};
+}
+
+/// # Running utilities
+///
+/// Provides a common trait to turn a type _"runnable"_ ([`RunnableItem`]).
+pub mod run {
+    pub use super::runnable_item::RunnableItem;
+}
